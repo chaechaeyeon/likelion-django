9fafb9e3d0548980ff269d710fe0d0b5d4ab9351
chaechaeyeon <00dreamadream@kakao.com>
2023-05-12 04:46:29 +0900

    week05

--- first-django/demos/views.py
+++ first-django/demos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.handlers.wsgi import WSGIRequest
+import random
 
 def calculator(request):
     #return HttpResponse('계산기 기능 구현 시작합니다')
@@ -27,3 +28,28 @@ def calculator(request):
     #3.응답
 
     return render(request, 'calculator.html',{'result':result})
+
+
+def lotto(request):
+    import random
+    
+    lotto_number = list()
+    for _ in range(7):
+        number= random.randint(1,45)
+        lotto_number.append(number)
+    
+    return render(request,'lotto.html',{'lotto_number':lotto_number})
+
+
+def lotto_index(request):
+    return render(request, 'lotto_index.html')
+
+def lotto_result(request):
+    lotto_number = list()
+    game = request.GET.get('game',1)
+    pull_number= [index for index in range(1,46)]
+
+    for _ in range(int(game)):
+        lotto_number.append(random.sample(pull_number,6))
+        
+    return render(request,'lotto_result.html',{'lotto_number':lotto_number, 'game':game})
--- liongram/config/urls.py
+++ liongram/config/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from posts.views import index, url_view, url_parameter_view,function_list_view,function_view, class_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('url/',url_view),
+    path('url/<str:username>/',url_parameter_view),
+    path('fbv/list',function_list_view),
+    path('fbv/', function_view),
+    path('cbv/', class_view.as_view()),
+    path('', index,name='index'),
+    path('posts/',include('posts.urls',namespace='posts')),
+]   
