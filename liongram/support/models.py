5d455ab9242699c5d19324db60b44db4307c6192
chaechaeyeon <00dreamadream@kakao.com>
2023-05-16 13:59:35 +0900

    week06

--- liongram/support/models.py
+++ liongram/support/models.py
@@ -18,3 +18,31 @@ class Faq(models.Model):
     updated_at : models.DateTimeField(verbose_name='최종 수정 일시',auto_now=True)
     created_by : models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='faq_created_by')
     updated_by : models.ForeignKey(to=User, on_delete=models.CASCADE,related_name='faq_updated_by')
+
+class Inquiry(models.Model):
+    CATEGORY_CHOICES = [
+        ('1','일반'),
+        ('2','계정'),
+        ('3','기타'),
+    ]
+
+    category = models.CharField(verbose_name='카테고리',max_length=2,choices=CATEGORY_CHOICES)
+    title = models.CharField(verbose_name='질문제목',max_length=80)
+    email= models.EmailField(verbose_name='이메일',blank=True)
+    phone= models.CharField(verbose_name='문자메시지',max_length=80)
+    is_imail= models.BooleanField(verbose_name='이메일 수신여부',default=False)
+    is_phone= models.BooleanField(verbose_name='문자메시지 수신여부',default=False)
+    content = models.TextField(verbose_name='문의내용')
+    image = models.ImageField(verbose_name='이미지',null=True,blank=True)
+
+    created_by : models.DateTimeField(to=User,on_delete=models.CASCADE,related_name='inquiry_created_by')
+    updated_by : models.DateTimeField(to=User, on_delete=models.CASCADE,related_name='inquiry_updated_by')
+
+class Answer(models.Model):
+    content= models.TextField(verbose_name='답변 내용')
+    created_at= models.DateTimeField(verbose_name='생성일지',auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='최종 수정 일지',auto_now=True)
+
+    Inquiry = models.ForeignKey(to='Inquiry',on_delete=models.CASCADE)
+    created_at = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='answer_created_by')
+    updated_at = models.ForeignKey(to=User,on_delete=models.CASCADE,related_name='answer_updated_by')
